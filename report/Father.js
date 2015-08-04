871fefa7d087ffd5da50e28de1ce53ea670336ad
xuxiaopu <xuxiaopuzi@126.com>
2015-08-04 14:57:40 +0800

    增加一个待测试变量
    待补充

--- report/Father.js
+++ report/Father.js
@@ -1,5 +1,5 @@
 casper.options.viewportSize = {width: 1366, height: 768};
-var value;
+var value,test1;
 
 casper.test.begin('Father.cpt', function(test) {
    casper.start('http://localhost:8075/WebReport/ReportServer?reportlet=demo%2Fparameter%2FFather.cpt');
